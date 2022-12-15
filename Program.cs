afd9408cb4aac59d1bd1e21ae51fa0434b2f4dcd
Denis <denis_hara@hotmail.com>
2022-12-15 10:41:04 -0300

    new

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿
+int -2.147.483.648 ~ 2.147.483.647
+uint 2.147.483.648 x2 
+long = - 9.223.372.036.854.775.808 ~ 9.223.372.036.854.775.807
+
+double = coordenadas 
+float menos preciso
+decimal = monetario / preciso
+
+Toint32 = bilhoes
+Toint64 = trilhoes
+----------------------------------------
+cast = casting
+
+int a = Convert.Toint32("5"); = virou string
+
+int a = int.Parse("5"); = transformou string em numero inteiro
+
+
+int a = Convert.Toint32(null); = 0
+já o
+int a = int.Parse(null); = error 
+-------------------------------------------
+String
+
+int inteiro = 5;
+string a = inteiro.ToString(); = 5; ToString transforma qualquer tipo em string
+-------------------------------------------
+Cast implícito
+
+int a = 5;
+double b = a;
+
+
+Console.WriteLine(a);
+
